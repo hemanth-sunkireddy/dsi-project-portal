d00b23593b7d8a29cc9c1f9a3bc9d026b6b096f2
mayankmittal29 <mayank.mittal@students.iiit.ac.in>
2024-11-09 15:38:12 +0530

    add doctor form page added

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import AddDoctor from './components/AddDoctor.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <AddDoctor />
   </React.StrictMode>
 );
