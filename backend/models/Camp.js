cf4fcc6e6483e4790ce64232a1c8398b61becb0b
Aishani Pandey <93460154+AishaniPandey@users.noreply.github.com>
2024-11-11 00:47:54 +0530

    removed unnecessary code

--- backend/models/Camp.js
+++ backend/models/Camp.js
@@ -1,17 +1,3 @@
-// const mongoose = require('mongoose');
-
-// const campSchema = new mongoose.Schema({
-//   schoolName: String,
-//   location: String,
-//   dateTime: Date,
-//   volunteer: String,
-//   doctor: String,
-//   contact: String,
-//   furtherDetails: String,
-// });
-
-// module.exports = mongoose.model('Camp', campSchema);
-
 const mongoose = require('mongoose');
 
 const { v4: uuidv4 } = require('uuid'); // Import uuidv4
