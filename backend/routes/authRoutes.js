f387107f8f8186adef0d6e885bca1ff1a96fc656
sujal-keshri <sujal.keshri@students.iiit.ac.in>
2024-11-19 04:33:15 +0530

    merg main branch

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -101,6 +101,14 @@ router.get('/screenings', async (req, res) => {
     res.json({ message: 'Failed to fetch Screenings', error });
   }
 });
+router.get('/screenings', async (req, res) => { 
+  try {
+    const screenings = await Screening.find(); 
+    res.status(200).json(screenings);
+  } catch (error) {
+    res.json({ message: 'Failed to fetch Screenings', error });
+  }
+});
 router.get('/profiledata/:name', async (req, res) => {
   const { name } = req.params;
   console.log(name)
